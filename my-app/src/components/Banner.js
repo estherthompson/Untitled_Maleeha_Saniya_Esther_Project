4a4dc64004be1d0f0ec141d190f56d2a33389c16
estherthompson <149338492+estherthompson@users.noreply.github.com>
2025-02-15 22:14:09 -0700

    dsjfndksjnf

--- my-app/src/components/Banner.js
+++ my-app/src/components/Banner.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Banner.css";
+import bannerBackground from "../assets/Background_Empty.png";
 
 function Banner() {
   return (
     <div className="banner">
-      <h1>Welcome to Beacon</h1>
+      <h1>a</h1>
     </div>
   );
 }
